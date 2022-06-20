1bf9a8d6faed97e7a826768638d2790e1fba425d
Schwimmerl <105862331+Schwimmerl@users.noreply.github.com>
2022-06-20 11:34:43 +0200

    Updated DATABASE
    Database geupdated, Treiber hinzugefügt,
    Menumusik in library Ordner verschoben

--- InfoschluesselSpiel/Database.java
+++ InfoschluesselSpiel/Database.java
@@ -15,13 +15,13 @@ public class Database
 	{
 		
 		//Zugangsdaten werden vorher festgelegt, um sie leichter ändern zu können.
-		String url = "jdbc:mysql5046.site4now.net";
+		String url = "jdbc:mysql5046.site4now.net/hisghcores";
 		String user = "a88aa4_hiscore";
 		String password = "Highscore1!";
 		
 		try 
 		{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver");  
 			//Verbindung wird versucht herzustellen, "try{}" verhindert eine Fehlermeldung bei fehlender Verbindung
 			con = DriverManager.getConnection(url, user, password);
 		}
@@ -34,7 +34,7 @@ public class Database
 	public void scoreSpeichern(String spieler, int Score)
 	{
 		try
-		{
+		{	//Fügt einen neuen Datensatz mit den Werten für Spielername und Score in die Datenbank ein
 			String insertSQLcode = "INSERT INTO db_a88aa4_hiscore.highscores(`ID`, `score`, `player`) VALUES (NULL, spieler, score)";
 			Statement stmt = con.createStatement();
 			stmt.execute(insertSQLcode);
