30097910b7c494678c6d381de56a0069a1a4518f
Schwimmerl <105862331+Schwimmerl@users.noreply.github.com>
2022-06-26 16:17:07 +0200

    Hab an der Datenbank rumgepfuscht

--- InfoschluesselSpiel/KeySpawner.java
+++ InfoschluesselSpiel/KeySpawner.java
@@ -9,8 +9,8 @@ public class KeySpawner {
 	private int spawnIteration;
 	private int startCountdown; 
 	public KeySpawner(){ // erzeugt 7 inaktive Schlüsseln die auf Spawn warten
-		keys = new Key[7];
-		for(int i = 0; i < 7; i++) {
+		keys = new Key[10];
+		for(int i = 0; i < 10; i++) {
 			keys[i] = new Key(0, 1500);
 		}
 	}
@@ -22,11 +22,11 @@ public class KeySpawner {
 		// Wenn der mindestzeitabstand zwischen spawns erreicht wurde
 		if(spawnTimerValue < 0) {
 			spawnIteration ++; 
-			if(keys[spawnIteration % 7].getUse() == false) // wird der nächste Schlüssel
+			if(keys[spawnIteration % 10].getUse() == false) // wird der nächste Schlüssel
 				// aus dem Array genommen und auf inaktivität überprüft
 				{ // Der inaktive Key bekommt zufällige Koordinaten
-				keys[spawnIteration % 7].koordinatenSetzen(zufallZahl(), 10);
-				spawnTimerValue = 10; // Mindestzeitabstand zwischen Spawns in FPS
+				keys[spawnIteration % 10].koordinatenSetzen(zufallZahl(), 10);
+				spawnTimerValue = 5; // Mindestzeitabstand zwischen Spawns in mSek
 			}
 		}
 	}
@@ -40,7 +40,7 @@ public class KeySpawner {
 	public void keysBewegen(Graphics graphics) //siehe Klasse Key
 	{
 		spawn();
-		for(int j = 0; j <7; j++) {
+		for(int j = 0; j <10; j++) {
 			keys[j].bewegen(graphics);
 		}
 	}
